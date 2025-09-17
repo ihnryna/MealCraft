b9e3cae24100d3d53bd70c2390eba7b9c3c75c75
ihnryna <151572051+ihnryna@users.noreply.github.com>
2025-09-17 12:27:26 +0300

    Initial commit

---
+++ src/main/java/org/l5g7/mealcraft/MealCraftApplication.java
@@ -0,0 +1,13 @@
+package org.l5g7.mealcraft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealCraftApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MealCraftApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/l5g7/mealcraft/MealCraftApplicationTests.java
@@ -0,0 +1,13 @@
+package org.l5g7.mealcraft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealCraftApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
