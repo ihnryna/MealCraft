5f8392fd64f46d913b3ea2d5e0412a06edcb494a
MaksymSemk <maksumsemen2006@gmail.com>
2025-09-30 14:46:20 +0300

    Move auth to new folder

--- src/main/java/org/l5g7/mealcraft/app/auth/Dto/RegisterUserDto.java
+++ src/main/java/org/l5g7/mealcraft/app/auth/Dto/RegisterUserDto.java
@@ -1,4 +1,4 @@
-package org.l5g7.mealcraft.dto;
+package org.l5g7.mealcraft.app.auth.Dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
--- src/main/java/org/l5g7/mealcraft/app/auth/Handler/AuthExceptionHandler.java
+++ src/main/java/org/l5g7/mealcraft/app/auth/Handler/AuthExceptionHandler.java
@@ -1,15 +1,12 @@
-package org.l5g7.mealcraft.handler;
+package org.l5g7.mealcraft.app.auth.Handler;
 
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.HashMap;
 import java.util.List;
