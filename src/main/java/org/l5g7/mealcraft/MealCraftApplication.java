688df32f982aad9e024a8c456d42d78f511c5ccc
vika204 <v.romaniuk@ukma.edu.ua>
2025-10-21 16:25:37 +0300

    add OpenAPI description and Swagger configuration

--- src/main/java/org/l5g7/mealcraft/MealCraftApplication.java
+++ src/main/java/org/l5g7/mealcraft/MealCraftApplication.java
@@ -1,8 +1,22 @@
 package org.l5g7.mealcraft;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(
+        info = @Info(
+                title = "MealCraft API",
+                version = "1.0.0",
+                description = "Це API для керування рецептами, інгредієнтами та покупками."
+        ),
+        servers = {
+                @Server(url = "http://localhost:8080", description = "Локальний сервер"),
+        }
+
+)
 @SpringBootApplication
 public class MealCraftApplication {
 
