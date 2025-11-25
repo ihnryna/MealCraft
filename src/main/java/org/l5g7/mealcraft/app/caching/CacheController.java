148894b7f7021361b44443192fa70073bbab07e5
MaksymSemk <maksumsemen2006@gmail.com>
2025-11-25 23:42:36 +0200

    added controller and service to manage cacheClearance

---
+++ src/main/java/org/l5g7/mealcraft/app/caching/CacheController.java
@@ -0,0 +1,23 @@
+package org.l5g7.mealcraft.app.caching;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@PreAuthorize("hasRole('ADMIN')")
+@Controller
+@RequestMapping("/cache")
+public class CacheController {
+    private final CacheService cacheService;
+
+    public CacheController(CacheService cacheService) {
+        this.cacheService = cacheService;
+    }
+
+    @GetMapping("/clear")
+    public ResponseEntity<?> clearCache() {
+        return ResponseEntity.ok(cacheService.clearCache());
+    }
+}
